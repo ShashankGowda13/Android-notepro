c9d3606b0d5a011cfe4abae328cd977d3ae20a89
Shashank Gowda <106665302+ShashankGowda13@users.noreply.github.com>
2023-05-26 15:27:45 +0530

    add

--- app/src/main/java/com/example/content/MainActivity.java
+++ app/src/main/java/com/example/content/MainActivity.java
@@ -10,9 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-<<<<<<< HEAD
+
 }
-=======
+
 }// shashank
->>>>>>> b296530 (add)
+
